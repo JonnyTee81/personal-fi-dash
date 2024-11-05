05894bea5a4630043b08175f52a7bec6ee03c7ad
JonnyTee81 <jthompson81@gmail.com>
2024-11-04 19:32:31 -0600

    Added vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'vercel.app'],
+  },
+  // Ensure Google Charts works
+  webpack: (config) => {
+    config.externals = [...config.externals, 'canvas', 'jsdom'];
+    return config;
+  },
+};
 
 export default nextConfig;
